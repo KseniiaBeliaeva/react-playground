6b3e572b2b20eebde992877a059b4058b1840df0
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-11-28 12:57:03 +1100

    Using state property

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import Person from "./Person/Person";
 import { Button } from 'react-bootstrap';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Max",
+        age: 28
+      },
+      {
+        name: "Stella",
+        age: 27
+      },
+      {
+        name: "Manu",
+        age:26
+      }
+    ]
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <Button bsStyle="primary">Switch name</Button>
-      <Person name="Max" age="28"/>
-      <Person name="Lidia" age="26">My hobbies: Racing </Person>
-      <Person name="Manu" age="29"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
