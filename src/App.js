7b13783ae14973e4ae7d023f27860a4d817c5dcc
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-11-28 12:47:43 +1100

    Add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person";
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi</h1>
+        <Button bsStyle="primary">Switch name</Button>
+      <Person name="Max" age="28"/>
+      <Person name="Lidia" age="26">My hobbies: Racing </Person>
+      <Person name="Manu" age="29"/>
       </div>
     );
   }
